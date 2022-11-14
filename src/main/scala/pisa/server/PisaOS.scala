ce9ba73da0696085a5b7e9decf7d2134da465907
mmikula <mikula.maciek@gmail.com>
2022-11-14 16:06:03 +0100

    Debug println.

--- src/main/scala/pisa/server/PisaOS.scala
+++ src/main/scala/pisa/server/PisaOS.scala
@@ -570,7 +570,7 @@ class PisaOS(var path_to_isa_bin: String, var path_to_file: String, var working_
 
     val f_st: Future[Unit] = Future.apply {
       Breaks.breakable {
-        for ((transition, text) <- parse_text(thy, isar_string).force.retrieveNow)
+        for ((transition, text) <- parse_text(thy1, isar_string).force.retrieveNow)
           continue.breakable {
             if (text.trim.isEmpty) continue.break
             // println("Small step: " + text)
