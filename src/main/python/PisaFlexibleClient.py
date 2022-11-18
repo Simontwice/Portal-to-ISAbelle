6506cf13cec00f8b56dffc4ca9423df76ac6fc75
mmikula <mikula.maciek@gmail.com>
2022-11-18 20:01:47 +0100

    Catch Inaccessible fact

--- src/main/python/PisaFlexibleClient.py
+++ src/main/python/PisaFlexibleClient.py
@@ -211,7 +211,7 @@ class IsaFlexEnv:
                 next_proof_state_clean = trim_string_optional(next_proof_state)
                 step_correct = True
                 for prefix_error in [
-                    "Step error: Undefined fact", "Step error: Bad fact"
+                    "Step error: Undefined fact", "Step error: Bad fact", "Step error: Inaccessible fact"
                 ]:
                     if prefix_error in next_proof_state_clean:
                         step_correct = False
@@ -280,7 +280,7 @@ class IsaFlexEnv:
                 next_proof_state_clean = trim_string_optional(next_proof_state)
                 step_correct = True
                 for prefix_error in [
-                    "Step error: Undefined fact", "Step error: Bad fact"
+                  "Step error: Undefined fact", "Step error: Bad fact", "Step error: Inaccessible fact"
                 ]:
                     if prefix_error in next_proof_state_clean:
                         step_correct = False
@@ -325,7 +325,7 @@ class IsaFlexEnv:
                 next_proof_state_clean = trim_string_optional(next_proof_state)
                 step_correct = True
                 for prefix_error in [
-                    "Step error: Undefined fact", "Step error: Bad fact"
+                    "Step error: Undefined fact", "Step error: Bad fact", "Step error: Inaccessible fact"
                 ]:
                     if prefix_error in next_proof_state_clean:
                         step_correct = False
