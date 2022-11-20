bcef38c1597cc0ebe6a2e698f8c18ed87834ef26
simontwice <szymon.antoniak1@gmail.com>
2022-11-20 16:58:37 +0100

    Revert "remove timeouts"
    This reverts commit 298f2a42c988ad926137e25ab70a5b2351a2983c.

--- src/main/python/PisaFlexibleClient.py
+++ src/main/python/PisaFlexibleClient.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 import grpc
 from absl import logging
 
+from func_timeout import func_set_timeout, FunctionTimedOut
 from typing import List
 
 from pisa.src.main.python import server_pb2, server_pb2_grpc
@@ -124,6 +125,7 @@ class IsaFlexEnv:
             print(e)
         return self.obs_string
 
+    @func_set_timeout(1800, allowOverride=True)
     def step_to_top_level_state(self, action, tls_name, new_name):
         obs_string = "Step error"
         done = False
@@ -149,6 +151,7 @@ class IsaFlexEnv:
     def get_proof_level(self,tls_name="default"):
         return self.post(f"<get_proof_level> {tls_name}")
 
+    @func_set_timeout(1800, allowOverride=True)
     def post(self, action):
         return self.stub.IsabelleCommand(server_pb2.IsaCommand(command=action)).state
 
@@ -183,9 +186,8 @@ class IsaFlexEnv:
         try:
             facts = self.post(f"<global facts and defs> {tls_name}")
             return facts
-        except:
-            facts = {}
-            raise NotImplementedError
+        except FunctionTimedOut:
+            raise AvailableFactsTimeout
 
     def dataset_extraction_global_facts(self, isabelle_state):
         """
@@ -299,6 +301,7 @@ class IsaFlexEnv:
         translated_premises = [step.split()[-1] for step in successful_steps]
         return translated_premises, unsuccessful_premises
 
+    @func_set_timeout(100, allowOverride=True)
     def initialise_toplevel_state_map(self):
         try:
             obs_string = self.stub.IsabelleCommand(
@@ -310,6 +313,7 @@ class IsaFlexEnv:
             print("**Unsuccessful initialisation**")
             raise InitFailedException
 
+    @func_set_timeout(500, allowOverride=True)
     def extract_theory_steps(self):
         all_steps_str = self.stub.IsabelleCommand(
             server_pb2.IsaCommand(command="PISA extract actions")
